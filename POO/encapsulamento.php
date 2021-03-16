b6f1ddc62e253cb2afb81b78d6bf191b21a56c79
gustavomelo20 <infogustavomelo@gmail.com>
2021-03-16 18:02:30 -0300

    encapsulamento

---
+++ POO/encapsulamento.php
@@ -0,0 +1,30 @@
+<?php
+
+class Pessoa{
+
+     public $nome = "Rasmus Lerdorf";
+     protected $idade = 48;
+     private $senha = "123456";
+     
+     
+     public function verDados(){
+          echo $this->nome . "<br/>";
+          echo $this->idade . "<br/>";
+          echo $this->senha. "<br/>";
+     }
+
+
+
+}
+
+
+$objeto = new Pessoa();
+
+$objeto->verDados();
+
+
+
+
+
+?>
+
